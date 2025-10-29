892b382e99364cefe4a435eb9d3a48b0a37cb1e7
Masooma-Ali <masoomaali347@gmail.com>
2025-10-29 23:29:33 +0500

    img name

--- klt/example3.c
+++ klt/example3.c
@@ -35,7 +35,7 @@ int main()
   tc->affineConsistencyCheck = -1;  /* set this to 2 to turn on affine consistency check */
  
   // ---- load first image from your dataset ----
-  img1 = pgmReadFile("src/V1-1/images_dataset/frame_0000.pgm", NULL, &ncols, &nrows);
+  img1 = pgmReadFile("src/V1-1/images_dataset/frame_0001.pgm", NULL, &ncols, &nrows);
   img2 = (unsigned char *) malloc(ncols * nrows * sizeof(unsigned char));
 
   KLTSelectGoodFeatures(tc, img1, ncols, nrows, fl);
