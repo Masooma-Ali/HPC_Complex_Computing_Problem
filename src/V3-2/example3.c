73ffece928a0637b370b6dfab7f7f9f466573dbc
Masooma-Ali <masoomaali347@gmail.com>
2025-10-30 08:30:27 +0500

    example3changed

--- src/V3-2/example3.c
+++ src/V3-2/example3.c
@@ -23,7 +23,7 @@ int main()
   KLT_TrackingContext tc;
   KLT_FeatureList fl;
   KLT_FeatureTable ft;
-  int nFeatures = 150, nFrames = 10;
+  int nFeatures = 150, nFrames = 168;
   int ncols, nrows;
   int i;
 
