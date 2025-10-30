c3d18959ec0776e07dd27befca983ba0fd58c17c
Masooma-Ali <masoomaali347@gmail.com>
2025-10-30 13:06:52 +0500

    new set

--- src/V1-1/example3.c
+++ src/V1-1/example3.c
@@ -1,8 +1,8 @@
 /**********************************************************************
 Finds the 150 best features in an image and tracks them through the 
-next two images.  The sequential mode is set in order to speed
-processing.  The features are stored in a feature table, which is then
-saved to a text file; each feature list is also written to a PPM file.
+sequence of images: frame_000320.pgm to frame_000600.pgm.
+The sequential mode is set in order to speed processing.
+The features are stored in a feature table and written to files.
 **********************************************************************/
 
 #include <stdlib.h>
@@ -19,42 +19,57 @@ int main()
 #endif
 {
   unsigned char *img1, *img2;
-  char fnamein[100], fnameout[100];
+  char fnamein[200], fnameout[200];
   KLT_TrackingContext tc;
   KLT_FeatureList fl;
   KLT_FeatureTable ft;
-  int nFeatures = 150, nFrames = 10;
+  int nFeatures = 150;
+  int startFrame = 320, endFrame = 600;
+  int nFrames = endFrame - startFrame + 1;
   int ncols, nrows;
-  int i;
+  int i, frame;
 
+  // Create tracking structures
   tc = KLTCreateTrackingContext();
   fl = KLTCreateFeatureList(nFeatures);
   ft = KLTCreateFeatureTable(nFrames, nFeatures);
+
   tc->sequentialMode = TRUE;
   tc->writeInternalImages = FALSE;
   tc->affineConsistencyCheck = -1;  /* set this to 2 to turn on affine consistency check */
- 
-  img1 = pgmReadFile("img0.pgm", NULL, &ncols, &nrows);
-  img2 = (unsigned char *) malloc(ncols*nrows*sizeof(unsigned char));
 
+  // Read the first image
+  sprintf(fnamein, "newset/frame_%06d.pgm", startFrame);
+  img1 = pgmReadFile(fnamein, NULL, &ncols, &nrows);
+  img2 = (unsigned char *) malloc(ncols * nrows * sizeof(unsigned char));
+
+  // Select good features from the first image
   KLTSelectGoodFeatures(tc, img1, ncols, nrows, fl);
   KLTStoreFeatureList(fl, ft, 0);
-  KLTWriteFeatureListToPPM(fl, img1, ncols, nrows, "feat0.ppm");
+  sprintf(fnameout, "feat_%06d.ppm", startFrame);
+  KLTWriteFeatureListToPPM(fl, img1, ncols, nrows, fnameout);
 
-  for (i = 1 ; i < nFrames ; i++)  {
-    sprintf(fnamein, "img%d.pgm", i);
+  // Track features through all subsequent frames
+  for (i = 1, frame = startFrame + 1; frame <= endFrame; i++, frame++) {
+    sprintf(fnamein, "newset/frame_%06d.pgm", frame);
     pgmReadFile(fnamein, img2, &ncols, &nrows);
+
     KLTTrackFeatures(tc, img1, img2, ncols, nrows, fl);
+
 #ifdef REPLACE
     KLTReplaceLostFeatures(tc, img2, ncols, nrows, fl);
 #endif
+
     KLTStoreFeatureList(fl, ft, i);
-    sprintf(fnameout, "feat%d.ppm", i);
+    sprintf(fnameout, "feat_%06d.ppm", frame);
     KLTWriteFeatureListToPPM(fl, img2, ncols, nrows, fnameout);
   }
+
+  // Save feature table results
   KLTWriteFeatureTable(ft, "features.txt", "%5.1f");
   KLTWriteFeatureTable(ft, "features.ft", NULL);
 
+  // Free memory
   KLTFreeFeatureTable(ft);
   KLTFreeFeatureList(fl);
   KLTFreeTrackingContext(tc);
